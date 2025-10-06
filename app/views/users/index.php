58d296d41718306ffe934b24ef2fb4b66ca82740
Cedrick <alabadocedrickloid@gmail.com>
2025-10-06 04:24:20 +0100

    x

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -84,7 +84,7 @@
             name="q" 
             value="<?=html_escape($_GET['q'] ?? '')?>" 
             placeholder="Search student..." 
-            class="px-4 py-2 border border-gray-400 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-64 bg-white placeholder-gray-400">
+            class="px-4 py-2 border border-gray-400 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-green-500 w-64 bg-white placeholder-gray-400 text-black">
           <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-r-lg shadow transition">
             <i class="fa fa-search"></i>
           </button>
